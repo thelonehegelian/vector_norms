32f925d39959b2e70c6fe784a58bac7a274dfd97
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 17:06:04 +0000

    Add l1, l2 and normalize function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use ndarray::{array, Array1, ArrayView1};
 fn main() {
-    println!("Hello, world!");
+    fn l1_norm(arr: Array1<f64>) -> f64 {
+        arr.fold(0.0, |acc, x| acc + x.abs())
+        // let sum = arr.mapv(|x| x.abs()).sum();
+    }
+    fn l2_norm(arr: Array1<f64>) -> f64 {
+        // arr.fold(0.0, |acc, x| acc + x.powi(2)).sqrt()
+        arr.dot(&arr).sqrt()
+    }
+    fn normalize(mut arr: Array1<f64>) -> f64 {
+        let norm = l2_norm(arr.clone());
+        arr.mapv_inplace(|x| x / norm);
+        norm
+    }
 }
